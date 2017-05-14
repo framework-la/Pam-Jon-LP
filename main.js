3275281d10de86d920bb9165ce0d53f4aee26a07
Jonathan Perera <jonathanperera@Jonathans-MacBook-Pro.local>
2017-05-14 14:59:39 -0700

    refactored logic for ajax calls

--- main.js
+++ main.js
@@ -1,91 +1,47 @@
-$("#submit").on('click', function (){
-    var question = $('#question').val()
-    //check if input is populated
-    if (question.length > 0) {
-        var num = Math.random() * 12;
-        //response is "Yes!""
-        if (num > 0 && num <=2) {
-            $.ajax({
+//ajax call to Giphy
+function giphyCall(answer, query) {
+    var callUrl = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC";
+    $.ajax({
             type: "GET",
-            url: "http://api.giphy.com/v1/gifs/search?q=yes&api_key=dc6zaTOxFJmzC",
+            url: callUrl,
             dataType: "json",
             success: function (data) {
                 var arraySelector = Math.floor(Math.random() * 25);
                 var source = data.data[arraySelector].images.fixed_height.url;
-                $('#answer').text('Yes!');
+                $('#answer').text(answer);
                 $('#image').attr('src', source);
+                
             }
         })
-        //response is "Yes, absolutely!""
-        } else if (num > 2 && num <=4) {
-            $.ajax({
-            type: "GET",
-            url: "http://api.giphy.com/v1/gifs/search?q=yes+absolutely&api_key=dc6zaTOxFJmzC",
-            dataType: "json",
-            success: function (data) {
-                var arraySelector = Math.floor(Math.random() * 25);
-                var source = data.data[arraySelector].images.fixed_height.url;
-                $('#answer').text('Yes, absolutely!');
-                $('#image').attr('src', source);
+};
+
+//calling Giphy function when submit button is pressed
+$("#submit").on('click', function () {
+        //check if input is populated
+        var question = $('#question').val().length;
+        if (question > 0) {
+            var num = Math.random() * 12;
+            //response is "Yes!"
+            if (num > 0 && num <=2){
+                giphyCall("Yes!", "yes");
+            //response is "Yes, absolutely!"
+            } else if (num > 2 && num <=4) {
+                giphyCall("Yes, absolutely!", "yes+absolutely");
+            //response is "Sounds good to me!"
+            } else if (num > 4 && num <= 6) {
+                giphyCall("Sounds good to me!", "sounds+good");
+            //response is "No"
+            } else if (num > 6 && num <= 8) {
+                giphyCall("No!", "no");
+            //response is "No, defintely not!"
+            } else if (num > 8 && num <= 10) {
+                giphyCall("No, definitely not!", "definitely+not");
+            //response is "Maybe..."
+            } else if (num > 10 && num <= 12) {
+                giphyCall("Maybe...", "maybe");
             }
-        })
-        //response is "Sounds good to me!"
-        } else if (num > 4 && num <= 6) {
-            $.ajax({
-            type: "GET",
-            url: "http://api.giphy.com/v1/gifs/search?q=sounds+good&api_key=dc6zaTOxFJmzC",
-            dataType: "json",
-            success: function (data) {
-                var arraySelector = Math.floor(Math.random() * 25);
-                var source = data.data[arraySelector].images.fixed_height.url;
-                $('#answer').text('Sounds good to me!');
-                $('#image').attr('src', source);
-            }
-        })
-        //response is "No"
-        } else if (num > 6 && num <= 8) {
-            $.ajax({
-            type: "GET",
-            url: "http://api.giphy.com/v1/gifs/search?q=no&api_key=dc6zaTOxFJmzC",
-            dataType: "json",
-            success: function (data) {
-                var arraySelector = Math.floor(Math.random() * 25);
-                var source = data.data[arraySelector].images.fixed_height.url;
-                $('#answer').text('No!');
-                $('#image').attr('src', source);
-            }
-        })
-        //response is "No, defintely not!"
-        } else if (num > 8 && num <= 10) {
-            $.ajax({
-            type: "GET",
-            url: "http://api.giphy.com/v1/gifs/search?q=definitely+not&api_key=dc6zaTOxFJmzC",
-            dataType: "json",
-            success: function (data) {
-                var arraySelector = Math.floor(Math.random() * 25);
-                var source = data.data[arraySelector].images.fixed_height.url;
-                $('#answer').text('No, definitely not!');
-                $('#image').attr('src', source);
-            }
-        })
-        //response is "Maybe..."
-        } else if (num > 10 && num <= 12) {
-            $.ajax({
-            type: "GET",
-            url: "http://api.giphy.com/v1/gifs/search?q=maybe&api_key=dc6zaTOxFJmzC",
-            dataType: "json",
-            success: function (data) {
-                var arraySelector = Math.floor(Math.random() * 25);
-                var source = data.data[arraySelector].images.fixed_height.url;
-                $('#answer').text("Maybe...");
-                $('#image').attr('src', source);
-            }
-        })
-    }
-// if input is not populated
-    } else {
-        $('#image').attr('src', '');
-        $('#answer').text('Please ask a question!')
-    }
-    
+        } else {
+            $('#image').attr('src', '');
+            $('#answer').text('Please ask a question!')
+        }
 })
